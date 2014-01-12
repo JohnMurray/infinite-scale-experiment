dc7256031ccc241c00739cf938b46541849b7221
John Murray <me@johnmurray.io>
2014-01-12 00:10:47 -0500

    Finished Phase 1
    + Added starter script to start servers with shard information
    + Updated Procfile to use new server-starter script
    
    + Updated router-server with config information regarding where
      certain keys should be sent, key-ranges, etc.
    + Updated router to actually route requests based on configs and
      keys and key-ranges
    
    + Updated campagin rackup to provide shard information from the
      environment variable.
    + Updated server with before hook to echo that the request was
      received.
    
    + Fixed bug in shard key - not properly checking key-methods
    + Updated readme with details on what will be in Phase 1 vs 2

--- phase-1/router-server.rb
+++ phase-1/router-server.rb
@@ -1,5 +1,9 @@
+require 'json'
 require 'bundler'
 Bundler.require(:default)
+
+require_relative 'shard/key-range'
+require_relative 'shard/key'
 
 class Router < Sinatra::Base
 
@@ -10,11 +14,41 @@ class Router < Sinatra::Base
   def initialize
     super()
 
+    @config = {
+      campaign: {
+        shards: [
+          [
+            Shard::KeyRange.new('a', 'm'),
+            {host: 'localhost', port: 3001}
+          ],
+          [
+            Shard::KeyRange.new('n', 'z'),
+            {host: 'localhost', port: 3002}
+          ]
+        ]
+      }
+    }
+
     [:get, :put, :post, :delete].each do |method|
-      Router.send(method, '*') do
-        "hello from method: #{method}"
+      Router.send(method, '/*/:shard') do
+        service = params[:splat].first
+        shard   = params[:shard]
+
+        if @config[service.to_sym]
+          match = @config[service.to_sym][:shards].select { |kr, _|
+            kr.include? (Shard::Key.new(shard))
+          }.first
+
+          if match 
+            to = match.last
+            STDERR.puts "Matched #{to}"
+            redirect "http://#{to[:host]}:#{to[:port]}/#{service}/#{shard}"
+          end
+        end
+
       end
     end
   end
+
 
 end
